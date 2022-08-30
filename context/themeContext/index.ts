718f47a4af8cc9619b990986f7da4aab2578b9c7
DeveloperOscar <161139@unamba.edu.pe>
2022-08-30 16:46:45 -0500

    eliminando carpetas con archivos deprecados

--- context/themeContext/index.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import darkTailwindCss from "themes/tailwindcss/dark";
-
-//modificar aqui la instancia para los distintos temas de colores
-const ThemeContext = React.createContext(darkTailwindCss);
-
-export default ThemeContext;
